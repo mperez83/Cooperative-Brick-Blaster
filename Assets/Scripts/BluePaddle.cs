a9e705cb71dc8b740ed8ccbab90a758e8a2846ba
Michael Perez <mperez980089@yahoo.com>
2019-03-03 01:34:30 -0800

    Added player scripts, created new scene for ME

---
+++ Assets/Scripts/BluePaddle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluePaddle : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveAmount = speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.MovePosition(new Vector2(transform.position.x - moveAmount, transform.position.y));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.MovePosition(new Vector2(transform.position.x + moveAmount, transform.position.y));
+        }
+    }
+}
---
+++ Assets/Scripts/RedPaddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedPaddle : MonoBehaviour
+{
+    public float speed = 1;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float moveAmount = speed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //transform.position += (Vector3.left * moveAmount);
+            rb.MovePosition(new Vector2(transform.position.x - moveAmount, transform.position.y));
+            //rb.velocity = Vector2.left * moveAmount;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            //transform.position += (Vector3.right * moveAmount);
+            rb.MovePosition(new Vector2(transform.position.x + moveAmount, transform.position.y));
+            //rb.velocity = Vector2.right * moveAmount;
+        }
+    }
+}
