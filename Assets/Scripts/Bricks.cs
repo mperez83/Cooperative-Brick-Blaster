f4415f7a9fb4f9b1d3a630116b37c84a22a567e4
dantheknight <dansdk.blue@gmail.com>
2019-03-05 18:43:28 -0800

    Bricks v0.1 - Initial Creation of Brick Prefab

---
+++ Assets/Scripts/Bricks.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bricks : MonoBehaviour
+{
+
+    public GameObject brickParticle;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(brickParticle, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
