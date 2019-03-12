fb4b84009c6748c78403f1751f78e56053afdeb6
Michael Perez <mperez980089@yahoo.com>
2019-03-11 23:43:54 -0700

    Started on powerups

---
+++ Assets/Scripts/PowerUpMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpMovement : MonoBehaviour
+{
+    public float fallSpeed = 6;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector2 moveAmount = new Vector2(0, -fallSpeed);
+        transform.Translate(moveAmount);
+
+        if (transform.position.y < GameMaster.instance.screenBottomEdge)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
