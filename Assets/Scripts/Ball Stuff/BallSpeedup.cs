3e10172400442d6354aadcc8e3728a4515755d49
Michael Perez <mperez980089@yahoo.com>
2019-04-30 14:44:50 -0700

    Added ball speedup overtime

---
+++ Assets/Scripts/Ball Stuff/BallSpeedup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpeedup : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float minMagnitude;
+    float curMagnitude;
+    public float maxMagnitude;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        curMagnitude = minMagnitude;
+    }
+
+    void Update()
+    {
+        if (curMagnitude < maxMagnitude)
+        {
+            curMagnitude += 0.1f * Time.deltaTime;
+            //print(curMagnitude);
+        }
+
+        if (rb.velocity.magnitude < minMagnitude)
+        {
+            Vector2 normalizedVelocity = rb.velocity.normalized;
+            rb.AddForce(normalizedVelocity);
+        }
+    }
+}
